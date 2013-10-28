ef9d45e8f1ca5da1d7e60407973636cb9bbdff25
Avishay Orpaz <avishorp@gmail.com>
2013-10-28 11:17:41 +0200

    Add failure handling to update_all_adaptors

--- host_software/libxser/src/abstract_xser_instance_manager.cpp
+++ host_software/libxser/src/abstract_xser_instance_manager.cpp
@@ -1,7 +1,10 @@
 #include <xser.h>
 #include "abstract_xser_instance.h"
+#include <boost/log/trivial.hpp>
+#include <exception>
 
 using namespace xser;
+using namespace std;
 
 void abstract_xser_instance_manager::update_all_adaptors()
 {
@@ -13,7 +16,15 @@ void abstract_xser_instance_manager::update_all_adaptors()
 	for(i=instances.begin(); i != instances.end(); i++) {
 		if (!(i->second->is_dfu_mode())) {
 			xser_instance_oper_ifx* ic = dynamic_cast<xser_instance_oper_ifx*>(i->second);
-			ic->set_com_display(ic->get_associated_com_number());
+
+			// Errors in this process are converted to warnings
+			try {
+				ic->set_com_display(ic->get_associated_com_number());
+			}
+			catch(runtime_error& e) {
+				BOOST_LOG_TRIVIAL(warning) << "set_com_display failed for " << ic->get_serial_number() << 
+					" :" << e.what();
+			}
 		}
 	}
 }
